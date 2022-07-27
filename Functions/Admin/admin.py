b50d68d1e9082a846f64febc29b7af4c4bf5a5b6
Mike Qiu <Michael.Qiu@discordapp.com>
2022-07-26 20:16:11 -0700

    Room unlocking works

--- Functions/Admin/admin.py
+++ Functions/Admin/admin.py
@@ -1,5 +1,7 @@
-from constants import ATTENDEE_ROLE_NAME, VALID_ROOM_ROLES
+from constants import ATTENDEE_ROLE_NAME, CARL_BOT_ID, VALID_ROOM_ROLES
 from discord.ext import commands
+from discord.member import Member
+from discord.message import Message
 from discord.role import Role
 import discord.utils
 
@@ -25,5 +27,62 @@ class Admin(commands.Cog):
         else:
             await ctx.send(f'Must specify a valid room role out of the following: {",".join(VALID_ROOM_ROLES)}')
 
+    @commands.Cog.listener()
+    async def on_member_update(self, before: Member, after: Member):
+        roles = [role.name for role in after.roles]
+        if 'room-3a-complete' in roles and 'room-3b-complete' in roles and 'room-3c-complete' in roles:
+            room_4_role: Role = discord.utils.get(after.guild.roles, name='room-4')
+            await after.add_roles(room_4_role, atomic=True)
+            # await after.send('Room 4 unlocked')
+        if 'room-5a-complete' in roles and 'room-5b-complete' in roles:
+            room_6_role: Role = discord.utils.get(after.guild.roles, name='room-6')
+            await after.add_roles(room_6_role, atomic=True)
+            # await after.send('Room 6 unlocked')
+
+    @commands.Cog.listener()
+    async def on_message(self, message: Message):
+        user_id = message.author.id
+        if user_id == CARL_BOT_ID:
+            channel = message.channel
+            guild = message.guild
+            if channel.name == 'room-1':
+                if message.content == "Room 1 complete":
+                    await self.unlock(guild, channel, 'room-2')
+            elif channel.name == 'room-2':
+                if message.content == "Room 2 complete":
+                    await self.unlock(guild, channel, 'room-3')
+            elif channel.name == 'room-3a':
+                if message.content == "Room 3a complete":
+                    await self.unlock(guild, channel, 'room-3a-complete')
+            elif channel.name == 'room-3b':
+                if message.content == "Room 3b complete":
+                    await self.unlock(guild, channel, 'room-3b-complete')
+            elif channel.name == 'room-3c':
+                if message.content == "Room 3c complete":
+                    await self.unlock(guild, channel, 'room-3c-complete')
+            elif channel.name == 'room-4':
+                if message.content == "Room 4 complete":
+                    await self.unlock(guild, channel, 'room-5')
+            elif channel.name == 'room-5a':
+                if message.content == "Room 5a complete":
+                    await self.unlock(guild, channel, 'room-5a-complete')
+            elif channel.name == 'room-5b':
+                if message.content == "Room 5b complete":
+                    await self.unlock(guild, channel, 'room-5b-complete')
+
+    async def unlock(self, guild, channel, room_name):
+        if room_name in VALID_ROOM_ROLES:
+            room_role: Role = discord.utils.get(guild.roles, name=room_name)
+            attendees_role: Role = discord.utils.get(guild.roles, name=ATTENDEE_ROLE_NAME)
+            for user in attendees_role.members:
+                await user.add_roles(room_role, atomic=True)
+            if room_name == 'room-3':
+                await channel.send('room-3a, room-3b, and room-3c unlocked. You must complete all 3 to unlock the next room.')
+            elif room_name == 'room-5':
+                await channel.send('room-5a and room-5b unlocked. You must complete both of them to unlock the next room.')
+            elif room_name not in ['room-3a-complete', 'room-3b-complete', 'room-3c-complete', 'room-5a-complete', 'room-5b-complete']:
+                await channel.send(f'{room_name} unlocked')
+        else:
+            await channel.send(f'Must specify a valid room role out of the following: {",".join(VALID_ROOM_ROLES)}')
 def setup(bot):
     bot.add_cog(Admin(bot))
--- constants.py
+++ constants.py
@@ -1,4 +1,6 @@
-ATTENDEE_ROLE_NAME = 'Escape Room Attendee'
+ATTENDEE_ROLE_NAME = "Escape Room Attendee"
+
+CARL_BOT_ID = 235148962103951360
 
 FOOD_TOUR_INTRO_MESSAGE = "Hey there! Wungus here. After a grueling day of work at the office, I was looking at" \
                           " places where I could grab a bite or drink on either 17th or De Haro. I had 4 places set " \
@@ -20,7 +22,12 @@ VALID_ROOM_ROLES = [
     'room-1',
     'room-2',
     'room-3',
+    'room-3a-complete',
+    'room-3b-complete',
+    'room-3c-complete',
     'room-4',
     'room-5',
+    'room-5a-complete',
+    'room-5b-complete',
     'room-6'
 ]
