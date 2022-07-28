bbe9be4122e237d752940a9c20425b8582b19cdf
Mike Qiu <Michael.Qiu@discordapp.com>
2022-07-27 17:12:09 -0700

    Update wungus food tour message

--- constants.py
+++ constants.py
@@ -3,10 +3,12 @@ ATTENDEE_ROLE_NAME = "Escape Room Attendee"
 CARL_BOT_ID = 235148962103951360
 
 FOOD_TOUR_INTRO_MESSAGE = "Hey there! Wungus here. After a grueling day of work at the office, I was looking at" \
-                          " places where I could grab a bite or drink on either 17th or De Haro. I had 4 places set " \
-                          "in stone, but I can't remember them for the life of me. You should be able to " \
-                          "extract the first word for each one of these places in this jumbled mess though: " \
-                          "'liapblrzmhielcobmhoknvi'. Would you please be so kind as to help me remember those 4 " \
+                          " places where I could grab a bite or drink right by the office on either 17th or De Haro. " \
+                          "I had 4 places set " \
+                          "in stone, but I can't remember them for the life of me. \n" \
+                          "You should be able to " \
+                          "extract the first word of each one of these 4 places in this jumbled mess though: " \
+                          "`liapblrzmhielcobmhoknvi`. Would you please be so kind as to help me remember those 4 " \
                           "places and deliver the food/beverage they are most known for to me? Thanks! Appreciate it!" \
                           "\n" \
                           "P.S. I only accept deliveries in the form of emoji reactions to this message."
